a7f3ee0557c78049783d134bd3d1d49c5e08f5c7
lomo321 <287410339@qq.com>
2015-09-07 12:02:14 +0800

    Site updated: 2015-09-07 12:02:14

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
  */
 require.config({
     paths: {
-        "jquery": "./com/jquery/jquery",
-        "velocity": "./com/velocity/velocity",
-        "velocity-ui": "./com/velocity/velocity.ui"
+        "jquery": "js/com/jquery/jquery",
+        "velocity": "js/com/velocity/velocity",
+        "velocity-ui": "js/com/velocity/velocity.ui"
     },
     shim: {
         "velocity": {
@@ -19,5 +19,38 @@ require.config({
 });
 
 require(["jquery"], function($) {
-    var motion
+    var motionON = function(){
+        require(["velocity","velocity-ui"],function(Velocity){
+            function loadBrand(){
+                var sequence = [
+                    {e:$('.line-left'),p:{translateY:'-200%'},o:{durantion:500}},
+                    {e:$('.line-right'),p:{translateY:'200%'},o:{durantion:500,sequenceQueue:false}},
+                    {e: $('.line-top'),p:{ translateX:'100%'}, o:{duration:500,}},
+                    {e: $('.line-bottom'),p:{ translateX:'-100%'}, o:{duration:500, sequenceQueue:false}},
+                    {e: $('.logo'),p:{ top:'0',opacity:1}, o:{duration:200}},
+                ];
+                sequence.push({e:$('.menu2'),p:{opacity:1},o:{duration:150}});
+                for(var i=0;i<4;i++) {
+                    sequence.push( {e: $('.menu-item')[i],p:{opacity:1},o:{duration:150}} );
+                }
+                $.Velocity.RunSequence(sequence);
+            }
+
+            loadBrand();
+        })
+    };
+    motionON();
+
+    var $topBar = $("#topBar");
+    window.onscroll = function() {
+        var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
+        if(scrollTop >= 130) {
+            $topBar.addClass("fixed");
+        } else {
+            $topBar.removeClass("fixed");
+        }
+    };
+    $("#title").on("click", function(){
+        $('html, body').animate({scrollTop:0}, 'slow');
+    });
 });
